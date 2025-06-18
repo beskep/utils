2ef8e1790f5f72fa4fe62f405631651f9730c6c6
beskep <beskep@naver.com>
2025-06-18 13:40:52 +0900

    refactor: enhance module import handling

--- __init__.py
+++ __init__.py
@@ -1,5 +1,43 @@
 from __future__ import annotations
 
-from . import cli, mpl, pl, terminal
+from typing import TYPE_CHECKING, NoReturn
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+
+class _MissingModule:
+    def __init__(self, module: str, dependencies: Iterable[str]) -> None:
+        self.module = module
+        self.dependencies = sorted(dependencies)
+        self.msg = f'Module `{module}` requires following packages: {self.dependencies}'
+
+    def __getattr__(self, name: str) -> NoReturn:
+        raise ImportError(self.msg)
+
+
+if TYPE_CHECKING:
+    from . import cli, mpl, pl, terminal
+else:
+    try:
+        from . import cli
+    except ImportError:
+        cli = _MissingModule('cli', ['cyclopts'])
+
+    try:
+        from . import mpl
+    except ImportError:
+        mpl = _MissingModule('mpl', ['cmap', 'matplotlib', 'seaborn'])
+
+    try:
+        from . import pl
+    except ImportError:
+        pl = _MissingModule('pl', ['polars', 'pyarrow', 'whenever', 'xlsxwriter'])
+
+    try:
+        from . import terminal
+    except ImportError:
+        terminal = _MissingModule('terminal', ['loguru', 'rich'])
+
 
 __all__ = ['cli', 'mpl', 'pl', 'terminal']

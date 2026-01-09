e97a8acae1fb32d83bddcd86dc4db47582375733
beskep <beskep@naver.com>
2026-01-09 10:35:31 +0900

    feat: lazy import

--- __init__.py
+++ __init__.py
@@ -1,48 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NoReturn
+import importlib
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from types import ModuleType
 
-
-class _MissingModule:
-    def __init__(self, module: str, dependencies: Iterable[str]) -> None:
-        self.module = module
-        self.dependencies = sorted(dependencies)
-        self.msg = f'Module `{module}` requires following packages: {self.dependencies}'
-
-    def __getattr__(self, name: str) -> NoReturn:
-        raise ImportError(self.msg)
-
-
-if TYPE_CHECKING:
     from . import cli, mpl, pl, terminal  # ty: ignore[unresolved-import]
     from .tqdm_rich import tqdm_rich as tqdmr  # ty: ignore[unresolved-import]
-else:
-    try:
-        from . import cli
-    except ImportError:
-        cli = _MissingModule('cli', ['cyclopts'])
-
-    try:
-        from . import mpl
-    except ImportError:
-        mpl = _MissingModule('mpl', ['cmap', 'matplotlib', 'seaborn'])
-
-    try:
-        from . import pl
-    except ImportError:
-        pl = _MissingModule('pl', ['polars', 'pyarrow', 'whenever', 'xlsxwriter'])
-
-    try:
-        from . import terminal
-    except ImportError:
-        terminal = _MissingModule('terminal', ['loguru', 'rich'])
-
-    try:
-        from .tqdm_rich import tqdm_rich as tqdmr
-    except ImportError:
-        tqdmr = _MissingModule('tqdm_rich', ['rich', 'tqdm'])
 
 __all__ = ['cli', 'mpl', 'pl', 'terminal', 'tqdmr']
+
+
+def __getattr__(name: str) -> ModuleType:
+    if name in __all__:
+        return importlib.import_module(f'.{name}', __name__)
+
+    msg = f"module '{__name__}' has no attribute '{name}'"
+    raise AttributeError(msg)
